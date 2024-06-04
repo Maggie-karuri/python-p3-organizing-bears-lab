7b95886a290adeab14ed4fbe24a18a158e347e65
Maggie-Karuri <karurimargaret111@gmail.com>
2024-06-05 02:36:52 +0300

    SQL: ADD, DELETE, SELECT, INSERT

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 7, 'M', 'White', 'Hot Tempered', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 21, 'M', 'Black', 'Cool', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 15, 'F', 'White', 'Hot Tempered', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 21, 'M', 'Black', 'Cool', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 10, 'F', 'White', 'Cool', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 29, 'M', 'Black', 'Cool', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 12, 'F', 'White', 'Hot Tempered', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, NULL, 'White', NULL, 1);
